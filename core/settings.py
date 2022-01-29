cba6348aece804eee3d8663d2b7f11544c8aa3b5
Leon van Zyl <leon.vanzyl@gmail.com>
2022-01-29 08:26:32 +0200

    Settings

--- core/settings.py
+++ core/settings.py
@@ -77,16 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'HOST': '',
-        'PORT': '',
-        'USER': '',
-        'PASSWORD': '',
+        'ENGINE': os.environ.get("DB_ENGINE"),
+        'NAME' : os.environ.get("DB_NAME"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
     }
 }
-
-
 
 
 # Password validation
